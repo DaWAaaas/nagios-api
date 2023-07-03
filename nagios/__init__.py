a966c5dd88666e5917b65d45936aecba70dc14ba
DaWAaaas <130453978+DaWAaaas@users.noreply.github.com>
2023-07-03 22:09:01 +0300

    Add files via upload

---
+++ nagios/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#
+# Nagios class.
+#
+
+version = "1.2.2"
+
+from core import *
---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import nagios
+
+setup(name='nagios-api',
+      version=nagios.version,
+      description='Control nagios using an API',
+      author='Mark Smith',
+      author_email='mark@qq.is',
+      license='BSD New (3-clause) License',
+      long_description=open('README.md').read(),
+      url='https://github.com/xb95/nagios-api',
+      packages=['nagios'],
+      scripts=['nagios-cli', 'nagios-api'],
+      install_requires=[
+        'diesel>=3.0',
+        'greenlet==0.3.4',
+        'requests'
+      ]
+     )
